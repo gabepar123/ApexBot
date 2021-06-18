f855ccd1acf770fbb2a6158afdb950cb4e7eadea
gabepar123 <gabepar123@gmail.com>
2021-06-18 17:31:38 -0400

    Initial commit

---
+++ src/main/java/RetroBot.java
@@ -0,0 +1,8 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class RetroBot {
+    public static void main(String[] args) throws Exception {
+        JDA jda = JDABuilder.createDefault("token").build();
+    }
+}
